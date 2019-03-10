7fcff871402e407771e3aa4dedaec34e9059238d
Pavel Koch <kouks.koch@gmail.com>
2019-03-10 11:03:02 +0000

    layout draft

--- src/app/feed/views/Home.vue
+++ src/app/feed/views/Home.vue
@@ -1,6 +1,100 @@
 <template>
   <div>
-    Hello
+    <main class="section">
+      <div class="container">
+        <div class="columns">
+          <div class="column is-2">
+            <h1 class="title is-1">Blobs</h1>
+            <p>
+              <a href="" class="is-size-6">#js</a>
+              <a href="" class="is-size-5">#node</a>
+              <a href="" class="is-size-4">#rust</a>
+              <a href="" class="is-size-5">#java</a>
+              <a href="" class="is-size-4">#aws</a>
+              <a href="" class="is-size-6">#serverless</a>
+              <a href="" class="is-size-3">#ai</a>
+            </p>
+          </div>
+
+          <div class="column is-5">
+            <article class="content">
+              <h2 class="title is-4"><span class="tag is-warning">new</span> Module std::option</h2>
+              <p class="subtitle is-6">
+                <a href="as">https://doc.rust-lang.org/std/option/index.html</a>
+              </p>
+              <p><small>#rust #functional</small></p>
+              <p>Type Option represents an optional value: every Option is either Some and contains a value, or None, and does not. Option types are very common in Rust code, as they have a number of uses...</p>
+            </article>
+
+            <hr>
+
+            <article class="content">
+              <h2 class="title is-4">Push API - Web APIs</h2>
+              <p class="subtitle is-6">
+                <a href="as">https://developer.mozilla.org/en-US/docs/Web/API/Push_API</a>
+              </p>
+              <p>The Push API gives web applications the ability to receive messages pushed to them from a server, whether or not the web app is in the foreground, or even currently loaded, on a user agent. This lets developers deliver asynchronous notifications and updates to users that opt in, resulting in better engagement with timely new content.</p>
+            </article>
+
+            <hr>
+
+            <article class="content">
+              <h2 class="title is-4">Carbon</h2>
+              <p class="subtitle is-6">
+                <a href="as">https://carbon.now.sh</a>
+              </p>
+              <p>Create and share beautiful images of your source code. Start typing or drop a file into the text area to get started.</p>
+            </article>
+          </div>
+
+          <div class="column is-5">
+            <article class="content">
+              <h2 class="title is-4">Module std::option</h2>
+              <p class="subtitle is-6">
+                <a href="as">https://doc.rust-lang.org/std/option/index.html</a>
+              </p>
+              <p><small>#rust #functional</small></p>
+              <p>Type Option represents an optional value: every Option is either Some and contains a value, or None, and does not. Option types are very common in Rust code, as they have a number of uses...</p>
+            </article>
+
+            <hr>
+
+            <article class="content">
+              <h2 class="title is-4">Push API - Web APIs</h2>
+              <p class="subtitle is-6">
+                <a href="as">https://developer.mozilla.org/en-US/docs/Web/API/Push_API</a>
+              </p>
+              <p>The Push API gives web applications the ability to receive messages pushed to them from a server, whether or not the web app is in the foreground, or even currently loaded, on a user agent. This lets developers deliver asynchronous notifications and updates to users that opt in, resulting in better engagement with timely new content.</p>
+            </article>
+
+            <hr>
+
+            <article class="content">
+              <h2 class="title is-4">Carbon</h2>
+              <p class="subtitle is-6">
+                <a href="as">https://carbon.now.sh</a>
+              </p>
+              <p>Create and share beautiful images of your source code. Start typing or drop a file into the text area to get started.</p>
+            </article>
+          </div>
+        </div>
+      </div>
+    </main>
+
+    <footer class="section">
+      <div class="container">
+        <div class="columns">
+          <div class="column is-2">
+          </div>
+          <div class="column">
+            1
+            <a href="">2</a>
+            <a href="">3</a>
+            <a href="">4</a>
+          </div>
+        </div>
+      </div>
+    </footer>
   </div>
 </template>
 
